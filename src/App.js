b9839183d68c316598a0b639f4f651ade873db26
Ajay-1502 <ahdesai40@gmail.com>
2025-07-29 11:47:43 +0530

    Created multiple slices and rendered components conditionally by subscribing to store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Fragment } from 'react/jsx-runtime';
+import { useSelector } from 'react-redux';
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
+import Auth from './components/Auth';
 import Counter from './components/Counter';
 
-
 function App() {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {auth ? <UserProfile /> : <Auth />}
+      <Counter />
+    </Fragment>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
 import classes from './Header.module.css';
+import { authActions } from '../store';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Header = () => {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {auth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
