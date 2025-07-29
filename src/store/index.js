a23266a880d0ee6c8b1d46ab5aceba3a07ddf8d8
Ajay-1502 <ahdesai40@gmail.com>
2025-07-29 16:22:31 +0530

    Refactor : Split the redux reducer slices to different components

--- src/store/index.js
+++ src/store/index.js
@@ -1,43 +1,6 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-
-const initialCounterState = { counter: 0, showToggle: true };
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    showToggle(state) {
-      state.showToggle = !state.showToggle;
-    },
-  },
-});
-
-const initialAuthState = { isAuthenticated: false };
-
-const authSlice = createSlice({
-  name: 'auth',
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
+import { configureStore } from '@reduxjs/toolkit';
+import counterSlice from './counter-slice';
+import authSlice from './auth-slice';
 
 const store = configureStore({
   reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
